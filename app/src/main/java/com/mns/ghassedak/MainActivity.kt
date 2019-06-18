6a0eeb47c251e9f71c5eb13ea047250887f6a6f9
Mehdi Nosrati Sahlan <mehdins@aut.ac.ir>
2019-06-18 20:40:34 +0430

    adds some cool stuff

--- app/src/main/java/com/mns/ghassedak/MainActivity.kt
+++ app/src/main/java/com/mns/ghassedak/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity(), JoyStick.JoyStickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         joyStick = binding.joystick
